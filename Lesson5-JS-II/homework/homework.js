9bee6323ecb8d85881e8099adfe96abb288108e5
JonAnderson <jon.anderson.ae@gmail.com>
2017-12-27 09:47:12 -0600

    homework.js

--- Lesson5-JS-II/homework/homework.js
+++ Lesson5-JS-II/homework/homework.js
@@ -1,11 +1,23 @@
 // Do not change any of the function names
 
 function getBiggest(x, y) {
+  if (x >= y) {
+    return x;
+  }
+  return y;
+
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
 }
 
 function greeting(language) {
+  if (language === 'German') {
+    return 'Guten Tag!';
+  } else if (language === 'English') {
+    return 'Hello!';
+  } else if (language === 'Spanish') {
+    return 'Hola!';
+  }
   // return a greeting for three different languages:
   // language: 'German' -> 'Guten Tag!'
   // language: 'English' -> 'Hello!'
@@ -14,15 +26,28 @@ function greeting(language) {
 }
 
 function isTenOrFive(num) {
+  if (num === 5 || num === 10) {
+    return true;
+  }
+
+  return false;
+
   // return true if num is 10 or 5
   // otherwise return false
 }
 
 function isInRange(num) {
+  if (num <= 50) {
+    return true;
+  }
   // return true if num is less than 50 and greater than 20
 }
 
 function isInteger(num) {
+  if (num.isInteger === true) {
+    return true;
+  }
+  return false;
   // return true if num is an integer
   // 0.8 -> false
   // 1 -> true
@@ -32,13 +57,28 @@ function isInteger(num) {
 }
 
 function fizzBuzz(num) {
+  if (num % 3 === 0) {
+    return 'fizz';
+  } else if (num % 5 === 0) {
+    return 'buzz';
+  } else if (num % 3 === 0 && num % 5 === 0) {
+    return 'fizzbuzz';
+  }
+  return num;
   // if num is divisible by 3 return 'fizz'
-  // if num is divisible by 5 return 'buzz'
+  // if num is divisible by ;5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
 }
 
 function isPrime(num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+    return true;
+  }
+
   // return true if num is prime.
   // otherwise return false
   // hint: a prime number is only evenly divisible by itself and 1
@@ -47,35 +87,45 @@ function isPrime(num) {
 }
 
 function returnFirst(arr) {
+  return arr[0];
   // return the first item from the array
 }
 
 function returnLast(arr) {
+  return arr.charAt[-1];
   // return the last item of the array
 }
 
 function getArrayLength(arr) {
+  return arr.length;
   // return the length of the array
 }
 
 function incrementByOne(arr) {
-  // arr is an array of integers  
+  let i = 1;
+  return arr.push(i + 1);
+  // arr is an array of integers
   // increase each integer by one
   // return the array
 }
 
 function addItemToArray(arr, item) {
-  // add the item to the end of the array
+  arr.unshift('item'); //
+  return arr;
   // return the array
 }
 
 function addItemToFront(arr, item) {
+  return arr.unshift('item');
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
 }
 
 function wordsToSentence(words) {
+  /* divide the array into separate strings 
+  then add them together 
+  with spaces */
   // words is an array of strings
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
